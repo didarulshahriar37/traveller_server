28bf353ec8317dd3d41ba691678d8f02f1a6f32e
Didarul Shahriar <didarul13579@gmail.com>
2026-01-31 17:41:58 +0600

    added destination collection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -48,7 +48,25 @@ async function run() {
             res.send(result);
         });
 
-        
+        // Destination related APIs
+        app.get('/popular-destinations', async (req, res) => {
+
+        });
+
+        app.get('/destinations', async (req, res) => {
+
+        });
+
+        app.post('/destinations', async(req, res) => {
+
+        });
+
+        app.delete('/destinations/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await destinationsCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
